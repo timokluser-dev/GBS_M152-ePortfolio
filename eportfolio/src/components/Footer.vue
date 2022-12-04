e91fc9f63269b6eafa594269c740cb896220d4d0
timokluser-dev <timo@timokluser.ch>
2022-12-04 15:26:48 +0100

    add footer

---
+++ eportfolio/src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer class="footer">
+    <div class="footer__inner">
+      <span class="footer__copyright">&copy; 2022 Timo Kluser</span>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({})
+export default class Footer extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.footer {
+  @apply h-footer-height w-full;
+  @apply bg-primary;
+
+  .footer__inner {
+    @apply h-full w-full;
+    @apply flex flex-col justify-center;
+
+    .footer__copyright {
+      @apply text-white;
+      @apply flex flex-row justify-center;
+    }
+  }
+}
+</style>
