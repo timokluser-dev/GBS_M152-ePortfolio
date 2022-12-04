7c144a3b871c92c8ccf61e18f5e8de0d2c08d6a5
timokluser-dev <timo@timokluser.ch>
2022-12-04 15:45:53 +0100

    update tailwind config & add fonts

--- eportfolio/src/main.ts
+++ eportfolio/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
+
+import '@fontsource/space-grotesk/index.css';
+import '@fontsource/space-mono/index.css';
 import './style.scss';
+
 import App from './App.vue';
 
 createApp(App).mount('#app');
--- eportfolio/tailwind.config.cjs
+++ eportfolio/tailwind.config.cjs
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jpacksx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Space Grotesk', 'sans-serif'],
+      mono: ['Space Mono', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#069748',
+        dark: '#231F20',
+        black: '#000000',
+        white: '#FFFFFF',
+      },
+      height: {
+        'header-height': '75px',
+        'footer-height': '75px',
+      },
+    },
   },
   plugins: [],
 };
