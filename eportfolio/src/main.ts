d1b4fa382a1a3d852d749b1c2f164e5100d01416
timokluser-dev <timo@timokluser.ch>
2022-12-18 13:31:09 +0100

    update vue entrypoint

--- eportfolio/src/main.ts
+++ eportfolio/src/main.ts
@@ -1,9 +1,30 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 import '@fontsource/space-grotesk/index.css';
 import '@fontsource/space-mono/index.css';
 import './style.scss';
 
 import App from './App.vue';
+import Page from './Page.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+const routes: RouteRecordRaw[] = [{ path: '/', name: 'home', component: Page }];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to, _, __) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth',
+      };
+    }
+  },
+});
+
+app.use(router);
+
+app.mount('#app');
