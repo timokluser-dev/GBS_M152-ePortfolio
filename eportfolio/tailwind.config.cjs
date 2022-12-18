106394e49d0bc6c8430283bbd067df74fee05345
timokluser-dev <timo@timokluser.ch>
2022-12-18 13:29:05 +0100

    update tailwind config

--- eportfolio/tailwind.config.cjs
+++ eportfolio/tailwind.config.cjs
@@ -1,23 +1,32 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      sans: ['Space Grotesk', 'sans-serif'],
-      mono: ['Space Mono', 'sans-serif'],
+      sans: ["Space Grotesk", "sans-serif"],
+      mono: ["Space Mono", "sans-serif"]
     },
     extend: {
       colors: {
-        primary: '#069748',
-        dark: '#231F20',
-        black: '#000000',
-        white: '#FFFFFF',
+        primary: "#069748",
+        dark: "#231F20",
+        black: "#000000",
+        white: "#FFFFFF",
+        overlay: "rgba(0, 0, 0, 0.3)"
       },
       height: {
-        'header-height': '75px',
-        'footer-height': '75px',
+        "header-height": "75px",
+        "footer-height": "75px"
       },
-    },
+      padding: {
+        "header-height": "75px",
+        "footer-height": "75px"
+      },
+      margin: {
+        "header-height": "75px",
+        "footer-height": "75px"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
